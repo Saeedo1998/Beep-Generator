2ec264cea45f791195568fab788d54b7162b4627
Saeedo1998 <Saeedo_98@hotmail.com>
2021-09-20 23:51:52 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+        class Program
+        {
+                static void Main(string[] args)
+                {
+                        Console.WriteLine("Welcome, please proceed to choose a program to run");
+                        Console.WriteLine("Program 1: Basic - Program 2: Advanced");
+                        Console.WriteLine("Select program to load: 1 - 2");
+                        int programToLoad = int.Parse(Console.ReadLine());
+                        if (programToLoad == 1)
+                        {
+                                //do nothing
+                        }
+                        else if (programToLoad == 2)
+                        {
+                                Program2.NotMain();
+                        }
+                        Console.Title = "Program 1 - Basic";
+
+                        int num1, num2, total;
+                        Console.WriteLine("Hello World!");
+                        
+                        Console.WriteLine("Enter number1:");
+                        num1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter number2:");
+                        num2 = int.Parse(Console.ReadLine());
+                        total = num1 + num2;
+                        Console.WriteLine("Total is: " + total);
+                        for (int i = 0; i < total; i++)
+                        {
+                                Random r = new Random();
+                                int duration, freq;
+                                freq = r.Next(500, 1000);
+                                duration = r.Next(800, 1000);
+                                Console.WriteLine("Beep Frequency: " + freq + "Hz - Beep Duration: " + duration + "ms");
+                                Console.Beep(freq, duration);
+
+                        }
+
+                }
+        }
+}
---
+++ ConsoleApp1/Program2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+        public class Program2
+        {
+                public static void NotMain()
+                {
+                        Console.Title = "Program 2 - Advanced";
+
+                        int freqMin, freqMax, durMin, durMax, numOfBeeps;
+                        Console.WriteLine("Hello World!");
+
+                        Console.WriteLine("Enter frequency min range in Hz:");
+                        freqMin = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter frequency max range in Hz:");
+                        freqMax = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter duration min range in ms:");
+                        durMin = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter duration max range in ms:");
+                        durMax = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Play how many beeps? (recommended 3 - 10)");
+                        numOfBeeps = int.Parse(Console.ReadLine());
+                        for (int i = 0; i < numOfBeeps; i++)
+                        {
+                                Random r = new Random();
+                                int duration, freq;
+                                freq = r.Next(freqMin, freqMax);
+                                duration = r.Next(durMin, durMax);
+                                Console.WriteLine("Beep Frequency: " + freq + "Hz - Beep Duration: " + duration + "ms");
+                                Console.Beep(freq, duration);
+
+                        }
+
+                }
+        }
+}
