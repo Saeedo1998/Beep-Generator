14d20dc8c4a710b433a22e6694e8b230c93a44f0
Saeedo1998 <the_gamer_98@hotmail.com>
2022-04-24 00:08:37 +0300

    Added validation while loop. Moved some functions to a different class.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,153 +4,149 @@ using System.Threading.Tasks;
 
 namespace BeepGenerator
 {
-        class Program
+    class Program
+    {
+
+        public static bool isFirstTimeRunningApp;
+        static void Main(string[] args)
         {
-                static void Main(string[] args)
+            try
+            {
+
+                //Console.BackgroundColor = ConsoleColor.White;
+                //Console.ForegroundColor = ConsoleColor.Black;
+
+                //RunCustomProgram1();
+                //int num = 0;
+                //while (true)
+                //{
+                //        Array s = new Array[];
+                //        Console.Write("-");
+                //        num++;
+                //        Thread.Sleep(100);
+                //        if (num == 10 || num == 20)
+                //        {
+                //                Console.WriteLine();
+                //                Thread.Sleep(1000);
+
+                //        }
+                //}
+                //Console.ForegroundColor = ConsoleColor.Green;
+                //Console.BackgroundColor = "#073605";
+                isFirstTimeRunningApp = true;
+
+                Console.Title = "Beep Generator";
+                string Welcome = "";
+                Console.Clear();
+                Console.WriteLine("--------------- " + nameof(Welcome) + " --------------- \r\n");
+                string[] lines = {
+                    "Please browse from the currently available programs:- \r\n",
+                    "(Program 1:- Basic) - (Program 2:- Advanced) - (Program 3:- Random) \r\n",
+                    "Enter the number for the desired Program: (1 - 3) \r\n"};
+
+                foreach (var line in lines)
                 {
-                        try
+                    if (isFirstTimeRunningApp)
+                    {
+                        //show text slowly
+                        CustomWrite.WriteSequentially(line, 10);
+                    }
+                    else
+                    {
+                        //show text normally
+                        Console.Write(line);
+                    }
+                }
+
+                int programToLoad = 1;
+
+                do
+                {
+                    //To-Do: validate user input properly and check if number is number or not
+                    int.TryParse(Console.ReadLine(), out programToLoad);
+                    if (programToLoad > 3)
+                    {
+                        Console.Error.Write("Invalid input");
+                        Console.WriteLine("");
+                        //Thread.Sleep(1750);
+                        //Restart();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                } while (true);
+
+
+                if (programToLoad == 1)
+                {
+                    Program1.Start();
+                    //after program finishes running restart main thread
+                    //DisplayRestartMessage();
+                    Restart();
+                }
+                else if (programToLoad == 2)
+                {
+                    Program2.Start();
+                    while (true)
+                    {
+                        Console.WriteLine("Restart program? (Y / N)");
+                        ConsoleKeyInfo userInput = Console.ReadKey();
+                        if (userInput.Key == ConsoleKey.Y)
                         {
-                                //Console.BackgroundColor = ConsoleColor.White;
-                                //Console.ForegroundColor = ConsoleColor.Black;
-
-                                //RunCustomProgram1();
-                                //int num = 0;
-                                //while (true)
-                                //{
-                                //        Array s = new Array[];
-                                //        Console.Write("-");
-                                //        num++;
-                                //        Thread.Sleep(100);
-                                //        if (num == 10 || num == 20)
-                                //        {
-                                //                Console.WriteLine();
-                                //                Thread.Sleep(1000);
-
-                                //        }
-                                //}
-                                //Console.ForegroundColor = ConsoleColor.Green;
-                                //Console.BackgroundColor = "#073605";
-                                Console.Title = "Beep Generator";
-                                string Welcome = "";
-                                Console.Clear();
-                                Console.WriteLine("--------------- " + nameof(Welcome) + " --------------- \r\n");
-                                WriteSequentially("Please browse from the currently available programs:- \r\n", 10);
-                                WriteSequentially("(Program 1:- Basic) - (Program 2:- Advanced) - (Program 3:- Derp) \r\n", 10);
-                                WriteSequentially("Enter the number for the desired Program: (1 - 3) \r\n", 10);
-
-                                int programToLoad = 0;
-                                int.TryParse(Console.ReadLine(), out programToLoad);
-
-                                if (programToLoad == 1)
-                                {
-                                        Program1.Start();
-                                        DisplayRestartMessage();
-                                        Restart();
-                                }
-                                else if (programToLoad == 2)
-                                {
-                                        Program2.Start();
-                                        while (true)
-                                        {
-                                                Console.WriteLine("Restart program? (Y / N)");
-                                                ConsoleKeyInfo userInput = Console.ReadKey();
-                                                if (userInput.Key == ConsoleKey.Y)
-                                                {
-                                                        Program2.Start();
-                                                }
-                                                else if (userInput.Key == ConsoleKey.N)
-                                                {
-                                                        break;
-                                                }
-                                        }
-                                        //DisplayRestartMessage();
-                                        Restart();
-                                }
-                                else if (programToLoad == 3)
-                                {
-                                        Program3.Start();
-                                        DisplayRestartMessage();
-                                        Restart();
-                                }
-                                else
-                                {
-                                        Console.Error.Write("Invalid input");
-                                        Thread.Sleep(1750);
-                                        Restart();
-                                }
-
-                                return;
+                            Program2.Start();
                         }
-                        catch (Exception e)
+                        else if (userInput.Key == ConsoleKey.N)
                         {
-                                Console.Error.WriteLine("ERROR: " + e.Message);
-                                throw;
+                            break;
                         }
+                    }
+                    //after program finishes running restart main thread
+                    //DisplayRestartMessage();
+                    Restart();
                 }
-                public static void WriteSequentially(string text)
+                else if (programToLoad == 3)
                 {
-                        foreach (var var in text)
-                        {
-                                Console.Write(var);
-                                WaitFor(90);
-
-                        }
-                }
-                public static void WriteSequentially(string text, int speedInMilliseconds)
-                {
-                        foreach (var var in text)
-                        {
-                                Console.Write(var);
-                                WaitFor(speedInMilliseconds);
-
-                        }
-                }
-                
-                public static void WriteSequentially(string text, TimeSpan time)
-                {
-                        foreach (var var in text)
-                        {
-                                Console.Write(var);
-                                WaitFor(time);
-
-                        }
-                }
-                
-                public static void DisplayRestartMessage()
-                {
-                        Console.WriteLine("Thank you, " + Environment.UserName.ToString() + ". " +
-                                        "Your cooperation is much appreciated");
-                        WaitFor(1750);
-                }
-                public static void Restart()
-                {
-                        //Restart app
-                        //Start process, friendly name is something like MyApp.exe (from current bin directory)
-                        System.Diagnostics.Process.Start(System.AppDomain.CurrentDomain.FriendlyName);
-
-                        //Close the current process
-                        Environment.Exit(0);
-                }
-                public static void RunCustomProgram1()
-                {
-                        Console.WriteLine("------------------------- Welcome to TROYA Turkish Airlines -------------------------");
-                        Console.ReadLine();
+                    Program3.Start();
+                    //after program finishes running restart main thread
+                    //DisplayRestartMessage();
+                    Restart();
                 }
 
-                public static void WaitFor(int milliseconds)
-                {
-                        //Thread.Sleep(speed);
-                        new System.Threading.ManualResetEvent(false).WaitOne(milliseconds);
-                        //Task.Delay(speed).Wait(); // Wait 2 seconds with blocking
-                        //await Task.Delay(2000); // Wait 2 seconds without blocking
-                }
 
-                public static void WaitFor(TimeSpan milliseconds)
-                {
-                        //Thread.Sleep(speed);
-                        new System.Threading.ManualResetEvent(false).WaitOne(milliseconds);
-                        //Task.Delay(speed).Wait(); // Wait 2 seconds with blocking
-                        //await Task.Delay(2000); // Wait 2 seconds without blocking
-                }
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("ERROR: " + e.Message);
+                throw;
+            }
         }
+
+        public static void DisplayRestartMessage()
+        {
+            Console.WriteLine("Thank you, " + Environment.UserName.ToString() + ". " +
+                            "Your cooperation is much appreciated");
+            CustomWrite.WaitFor(1750);
+        }
+
+        public static void Restart()
+        {
+            //Restart app
+            //Start process, friendly name is something like MyApp.exe (from current bin directory)
+            isFirstTimeRunningApp = false;
+
+            System.Diagnostics.Process.Start(System.AppDomain.CurrentDomain.FriendlyName);
+
+            //Close the current process
+            //Environment.Exit(0);
+
+        }
+        public static void RunCustomProgram1()
+        {
+            Console.WriteLine("------------------------- Welcome to TROYA Turkish Airlines -------------------------");
+            Console.ReadLine();
+        }
+
+    }
 }
